4fe066752ea36601b74a8f7d11c43d17d2a0383e
Sakshisak <sakshisa@iitk.ac.in>
2020-04-11 02:43:31 +0530

    Decrypt function added

--- RC4/rc4.py
+++ RC4/rc4.py
@@ -1,8 +1,23 @@
 '''
 RC4 Encryption
----Symmetric Key Encryption
+---Symmetric Key Encryption---
 '''
 
+
+def dec_string(text):
+    decrypt = ""
+    for i in range(0,len(text),2):
+        k = int(text[i:i+2],16)
+        #print(k)
+        decrypt += chr(k)
+    return decrypt
+
+def listToSttring(lst):
+    text = ""
+    for i in lst:
+        #print(chr(i))
+        text += chr(i)
+    return text
 
 def KSA(key):
     l = len(key)
@@ -23,8 +38,30 @@ def PRGA(S,plaintext):
         j = (j+S[i])%256
         S[i],S[j] = S[j], S[i]
         K =  S[(S[i]+S[j])%256]
+        #print(plaintext[n] , K)
         encrypted.append(ord(plaintext[n])^K)
     return encrypted
+
+def encrypt(key,plaintext):
+    S = KSA(key)
+    cipher = PRGA(S,plaintext)
+    #print("cipher", cipher)
+    res = ""
+    for s in cipher:
+        #print(hex(s).zfill(4))
+        res+=(hex(s)[2:]).zfill(2)
+    #print("Ciphertext is : ",res)
+    return res
+
+def decrypt(key,plaintext):
+    lst = dec_string(plaintext)
+    #print(lst)
+    S = KSA(key)
+    dec_list = PRGA(S,lst)
+    #print(dec_list)
+    init = listToSttring(dec_list)
+    #print(init)
+    return init
 
 '''
 key = "Key"
@@ -45,11 +82,9 @@ key = input("Enter key : ")
 plaintext = input("Enter plaintext : ")
 
 key = [ord(s) for s in key]
-S = KSA(key)
-cipher = PRGA(S,plaintext)
-#print("cipher", cipher)
-res = ""
-for s in cipher:
-    #print(hex(s).zfill(4))
-    res+=(hex(s)[2:]).zfill(2)
-print("Ciphertext is : ",res)
+
+encrypted = encrypt(key,plaintext)
+print("Encrypted : ", encrypted)
+
+decrypted = decrypt(key,encrypted)
+print("Decrypted : ", decrypted)
